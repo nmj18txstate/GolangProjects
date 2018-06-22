9a77d9e077764cf6f941a5b6dea5ce063a22c711
Nityam <nmj18@txstate.edu>
2018-06-22 10:55:01 -0500

    enjoy

--- 05_package/main/main.go
+++ 05_package/main/main.go
@@ -1,12 +1,11 @@
 package main
-
 import (
 	"fmt"
-	"github.com/nmj18txstate/GolangProjects/05_package/stringutil"
+	"github.com/GolangProjects/05_package/stringutil"
 	//"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 	//"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	//"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
 func main() {
